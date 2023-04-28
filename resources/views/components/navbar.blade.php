158777fd81e98ab560cc435fbed47e50c87a0efa
siusiusiublack1 <siusiusiublack@gmail.com>
2023-04-28 15:21:29 +0800

    addViewfolder

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,26 @@
+ 
+
+<nav class="navbar navbar-default bg-light"> 
+
+    <div class="container-fluid"> 
+    
+    <div class="navbar-header "> 
+    
+    <a class="navbar-brand " href="/student">Ricky's Crud Site</a> 
+    
+    </div> 
+    
+    <ul class="nav "> 
+    
+    <li class="nav-item" style="margin-right:20px;"><a href="/student" style="text-decoration:none;">index</a></li> 
+    
+    <li class="nav-item"><a href="/student/create" style="text-decoration:none;">Create</a></li> 
+    
+     
+     
+    
+    </ul> 
+    
+    </div> 
+    
+    </nav> 
